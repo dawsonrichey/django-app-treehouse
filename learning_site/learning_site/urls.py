32e4b59b48552b627e3d23018b2e4ada21f74c8e
Dawson Richey <dawson.richey@ttu.edu>
2020-08-14 01:01:13 -0500

    new wordcounter project init

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -13,23 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
-
-# from django.conf.urls import include, url
-# from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path(r'^admin/', include(admin.site.urls)),
-    path(r'^$', views.hello_world),
+    url(r'^courses/', include('courses.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.hello_world),
 ]
-
-# urlpatterns = [
-#     url(r'^admin/', include(admin.site.urls)),
-#     url(r'^$', views.hello_world),
-# ]
-
-
